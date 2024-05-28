d603087a6e420548bde6b04466a4531b829a0374
darshitkansara94 <48837243+darshitkansara94@users.noreply.github.com>
2024-05-28 09:01:15 +0530

    Add files via upload

---
+++ Introduction.js
@@ -0,0 +1,19 @@
+// Javascript Introduction :
+    // Javascript is scripting languange.
+    // It is client side lang.
+    // Used for validation, Arithmetic operations like AudioDestinationNode, Multiplication, Substraction and Division
+    // Whole page is not refreshed while update value in single control.
+    // Javascript file extension is ".js".
+
+// Javascipt Assignemnt :
+//     Internal :
+//         Used in HTML page it self.
+//         Used "<script></script>" tag for create javascript in HTML page.
+//         JS code is written in Head section and Body section. (Preferred to write JS code in head section).
+//     External :
+//         Used as a seperate JS file.
+//         Extension for create new JS file is ".js".
+//         Code reusability while create external file.
+//         JS file referenace in head section or body section of HTML page. (Preferred to write JS code in head section).
+//         To add referenace "<script src="JS file path">".
+
