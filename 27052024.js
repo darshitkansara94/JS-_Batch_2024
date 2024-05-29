cfd6176e481a21d064f5e2cc774dc7b6234a5a1d
darshitkansara94 <48837243+darshitkansara94@users.noreply.github.com>
2024-05-29 09:00:36 +0530

    Add files via upload

--- 27052024.js
+++ 27052024.js
@@ -1,1 +1,11 @@
-alert("This is First Application");
+// alert():
+    // alert is JS inbuilt function.
+    // alert is used to display information and warning or error to the user.
+    // Once alert dialog is open user have to perform action (Need to click on OK button).
+    // If dialog is open page is not process further actions until dialog is closed.
+
+    // Syntax :
+    //     alert("string"/ variable);
+
+    // Example :
+        alert("This is First Application");
