61d35a5f289806cf9d40f71ca2c3faf25ff93e2d
darshitkansara94 <48837243+darshitkansara94@users.noreply.github.com>
2024-05-29 08:59:59 +0530

    Add files via upload

---
+++ ConsoleLog.js
@@ -0,0 +1,11 @@
+// console.log() :
+//     Inbuilt fuction of JS.
+//     console.log() is used to display message in developer tool -> console tab.
+//     Process is not stop / user action is not required while using console.log().
+//     Do not use log in production / testing environment if not required. 
+
+//     Synatx:
+//         console.log("string" / variable);
+    
+//     Example :            
+            console.log("This is console");
