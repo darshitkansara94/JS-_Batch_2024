4a07da465b6513d8f45bee93c7b6acc333da6092
darshitkansara94 <48837243+darshitkansara94@users.noreply.github.com>
2024-05-30 09:01:55 +0530

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,42 @@
+// DataType :
+//     Convert a value into a particular format / type.
+
+//     Datatype in JS :
+//         var:
+//             var is used in older version of JS.
+//             Microsoft recommanded to use var not much.
+//         let :
+//             let is laternate of var.
+//             To replace var, let is introduced.
+//             Better memory management.
+//             Once variable is out of scope memory is released for variable.
+//         const :
+//             When your value is static that time you can use const.
+//             We can not change the value of const var at the runtime.
+
+
+// Variable :
+//     Variable is a identification of value.
+//     variable name can not start with special char.
+//     variable name should be in camel case.
+//     Space is not allowed in variable name.
+
+
+// Example :
+    // Declaration
+    var cde;
+
+    // Intilization
+    cde = "WelTech1";
+
+    // Declare with Intilization
+    var abc = "WelTech";
+
+    var intValue = 10;
+
+    // Declare and Intilization with blank value.
+    var cde = "";
+
+    console.log(abc);
+    console.log(typeof(abc));
+    console.log(typeof(intValue));
