8d49358b7e443c1b29c18478166b21571eec09b4
darshitkansara94 <48837243+darshitkansara94@users.noreply.github.com>
2024-06-05 09:06:18 +0530

    Add files via upload

---
+++ confirm.js
@@ -0,0 +1,23 @@
+// confirm :
+//     Confirm dialog is to get a confirmation from user.
+//     Contains two button : Ok and Cancel.
+//     Ok button return true.
+//     Cancel button return false.
+
+//     Syntax :
+//         confirm("Message to be display");
+
+var optionSelected = confirm("Are you sure you want to delete?");
+alert(optionSelected);
+
+if(confirm("Are you sure you want to delete the value?")){
+    alert("Clicked on Ok button");
+}
+else{
+    alert("Clicked on Cancel Button");
+}
+
+
+// Assignment :
+    // Decide grade based on user input in prompt.
+
