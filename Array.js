aae17802544f2746a623876817cbacee0c6bfa77
darshitkansara94 <48837243+darshitkansara94@users.noreply.github.com>
2024-06-18 20:31:44 -0700

    Update Array.js

--- Array.js
+++ Array.js
@@ -46,3 +46,98 @@ bikes[1] = "Pulsar";
 
 console.log("Bike values = " + bikes); // Concat array value with static string.
 
+// Display array single value in label
+document.getElementById('lblArrayVal').innerText = bikes[2];
+
+// push
+// pop
+// concat
+// join
+// at
+
+const arrayPushandPop = [];
+// Push :
+// Push is insertion of value into the Array.
+// It perform LIFO(Last in First Out) algorithm.
+
+// Syntax :
+//     Array.push(value);
+
+// Example :
+arrayPushandPop.push('Pizza', 'Burger');
+
+console.log(arrayPushandPop);
+
+// Pop :
+// Remove last inserted value from array.
+// No argument is required.
+
+// Syntax :
+//     Array.pop();
+
+// Example :
+arrayPushandPop.pop();
+
+console.log(arrayPushandPop);
+
+const dynamicArray = [];
+
+function EnterValueInArray() {
+    let arrayValue = document.getElementById("txtArrayValue").value;
+
+    dynamicArray.push(arrayValue);
+
+    document.getElementById('lblValue').innerText = arrayValue;
+
+    console.log(dynamicArray);
+}
+
+function RemoveValueFromArray() {
+    dynamicArray.pop();
+
+    console.log(dynamicArray);
+}
+
+// Join :
+// Convert array into String format and add seperator between each value.
+// Comma seperated value can be anything.
+
+// Syntax :
+//     array.join('seperator');
+
+// Example :
+const fruitArray = ['Orange', 'Mango', 'Kiwi', 'Apple'];
+
+console.log(fruitArray.join(' / '));
+
+console.log(typeof (fruitArray.join('/')));
+
+// Concat :
+// To merge two array into single array.
+
+// Syntax :
+//     array1.concat(array2);
+
+// Example :
+const vegetableArray = ['Tomato', 'Potato', 'Chilli', 'garlic'];
+
+console.log(fruitArray.concat(vegetableArray));
+
+console.log(fruitArray);
+
+// at :
+// Retrieve value from particular index.
+
+//Syntax :
+// array.at(index)
+
+// array.at[index]
+
+//Example :
+console.log(vegetableArray.at(1));
+
+// sort
+// reverse
+// delete
+
+
